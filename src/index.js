c85d76c1afe00a68c694163a1eabb64f236ad4f5
AVor0n <AVor0n@yandex.ru>
2021-03-05 01:08:45 +0400

    task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if (array[0] === undefined) {
+    return 0;
+  }
+  else {
+    return array.reduce((min, cur) => (cur < min) ? cur : min, array[0]);
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if (array[0] === undefined) {
+    return 0;
+  }
+  else {
+    return array.reduce((max, cur) => (cur > max) ? cur : max, array[0]);
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if (array[0] === undefined) {
+    return 0;
+  }
+  else {
+    return array.reduce((sum, cur) => cur + sum, 0) / array.length;
+  }
 }
